25b80ac8f2ade500dcb0e771f28a8bda41fe28a9
yakose <yakose.soa@gmail.com>
2020-04-18 14:50:01 +0200

    Add DateUtils and Ant build

---
+++ src/dk/yakose/utils/DateUtils.java
@@ -0,0 +1,18 @@
+package dk.yakose.utils;
+import java.util.Date;
+
+public class DateUtils {
+
+	public static void main(String[] args) {
+
+		System.out.println(getLocalCurrentDate());
+		
+	}
+
+	private static Date getLocalCurrentDate() {
+	
+		return new Date();
+		
+	}
+
+}
